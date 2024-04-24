aed6634b17f373902c36a972e3e83e6048188c82
Hayden Isler <github@haydenisler.com>
2024-04-24 06:52:56 -0500

    shadcn setup + content nav

--- xivapi-explorer/src/App.tsx
+++ xivapi-explorer/src/App.tsx
@@ -1,162 +1,96 @@
-import { useState, ReactNode, useMemo, ChangeEvent } from 'react';
-import useSWR, { SWRConfig } from 'swr';
+import { Suspense, useMemo, useState } from "react";
+import useSWR from "swr";
+
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator"
 
 
-import './App.scss';
 
-// --- Constants ---
+const XIVAPI_BASE_URL = "https://xivapi.com";
 
-const XIVAPI_BASE_ROUTE = 'https://xivapi.com';
-
-// --- API Types ---
-
-interface Pagination {
-  Page: number;
-  PageNext: number | null;
-  PagePrev: number | null;
-  PageTotal: number;
-  Results: number;
-  ResultsPerPage: number;
-  ResultsTotal: number;
-}
-
-interface ContentResult {
-  ID: number;
-  Icon: string | null;
-  Name: string | null;
-  Url: string;
-}
-
-interface ContentResponse {
-  Pagination: Pagination;
-  Results: ContentResult[];
-}
-
-// --- Utility Components ---
-
-interface RequestHandlerProps {
-  isLoading: boolean;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  error: any;
-  children?: ReactNode | undefined;
-}
-
-function RequestHandler ({ isLoading, error, children }: RequestHandlerProps): ReactNode {
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  if (error) {
-    return <div>{error}</div>
-  }
-
+function Loading() {
   return (
-    <>
-      {children}
-    </>
-  );
-}
-
-// --- ContentItem ---
-
-interface ContentItemProps extends ContentResult {}
-
-function ContentItem({ ID: id, Icon: icon, Name: name, Url: url }: ContentItemProps) {
-  const [open, setOpen] = useState(false);
-
-  const { data: content, isLoading, error } = useSWR<ContentResponse>(open ? `${XIVAPI_BASE_ROUTE}${url}` : null);
-
-  const handleClick = () => setOpen((prev) => !prev);
-
-  return (
-    <div className="content-item" onClick={handleClick}>
-      <div>{id}</div>
-      {icon ? (<img src={`${XIVAPI_BASE_ROUTE}${icon}`} />) : (<span>-</span>) }
-      {name ? (<div>{name}</div>) : (<span>-</span>)}
-      <div>{url}</div>
-      <div className="content-item__data">
-        {open ? (
-          <RequestHandler isLoading={isLoading} error={error}>
-            <pre>{JSON.stringify(content, null, 2)}</pre>
-          </RequestHandler>
-        ) : null}
-      </div>
+    <div className="h-full flex items-center">
+      <p className="text-sm text-muted-foreground">
+        Loading...
+      </p>
     </div>
   );
 }
 
-// --- ContentArea ---
 
-interface ContentAreaProps {
-  data: string;
+function Content() {
+  return (
+    <div className="flex-shrink-0 flex-grow bg-slate-200"></div>
+  );
 }
 
-function ContentArea({ data }: ContentAreaProps) {
-  const [open, setOpen] = useState(false);
-
-  const { data: content, isLoading, error } = useSWR<ContentResponse>(open ? `${XIVAPI_BASE_ROUTE}/${data}` : null);
-
-  const handleClick = () => setOpen((prev) => !prev);
-
+function SidebarRow({ content }: { content: string; url?: string; }) {
   return (
-    <div className="content-area">
-      <div className="content-area__title" onClick={handleClick}>
-        { data }
-      </div>
-      {open ? (
-        <div className="content-area__results">
-          <div className="content-area__header">
-            <div>id</div>
-            <div>icon</div>
-            <div>name</div>
-            <div>url</div>
-          </div>
-          <RequestHandler isLoading={isLoading} error={error} >
-            {content?.Results.map((result) => <ContentItem key={result.ID} {...result} />)}
-          </RequestHandler>
-        </div>
-      ) : null}
+    <div className="w-full border rounded-sm px-2 py-1 mb-2 hover:cursor-pointer hover:shadow hover:bg-slate-50 transition-all">
+      <span>{content}</span>
     </div>
   );
 }
 
-// --- Main ---
+function Sidebar() {
+  const { data: gameContent } = useSWR<string[]>(`${XIVAPI_BASE_URL}/content`, null, { suspense: true });
 
-function Main() {
   const [search, setSearch] = useState('');
 
-  const { data: content, isLoading, error } = useSWR(`${XIVAPI_BASE_ROUTE}/content`);
-
   const filteredContent = useMemo(() => {
-    if (!content) return content;
-
-    return content.filter((c: string) => c.toLowerCase().includes(search.toLowerCase()));
-  }, [content, search]);
-  
-  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  } 
+    return gameContent?.filter((c) => c.toLowerCase().includes(search.toLowerCase()));
+  }, [gameContent, search]);
 
   return (
-    <>
-      <input type="text" value={search} onChange={handleSearch}></input>
-      <RequestHandler isLoading={isLoading} error={error}>
-        {filteredContent?.map((area: string) => 
-          <ContentArea key={area} data={area} />
-        )}
-      </RequestHandler>
-    </>
+    <div className="flex-shrink-0 border-r h-full px-4 pt-4 w-80">
+      <Suspense fallback={<Loading />}>
+        <Input 
+          type="text" 
+          placeholder="Search..." 
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Separator className="my-4" />
+        <ScrollArea className="h-full">
+          {filteredContent?.map((c: string) => <SidebarRow content={c} />)}
+        </ScrollArea>
+      </Suspense>
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <div className="flex-shrink-0 flex-grow flex h-0">
+      <Sidebar />
+      <Content />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="w-full flex-shrink-0 p-4 border-b">
+      <h1>xivapi explorer</h1>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div className="w-screen h-screen overflow-hidden flex flex-col">
+      <Header />
+      <Main />
+    </div>
   );
 }
 
 function App() {
   return (
-    <SWRConfig value={{
-      fetcher: (resource, init) => fetch(resource, init).then(res => res.json()),
-    }}>
-      <Main />
-    </SWRConfig>
+    <Layout />
   );
 }
 
-export default App
+export default App;
--- xivapi-explorer/src/main.tsx
+++ xivapi-explorer/src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import '@fontsource/iosevka';
 
-import './index.scss';
+import{ SWRConfig } from 'swr';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SWRConfig value={{
+      fetcher: (resource, init) => fetch(resource, init).then(res => res.json()),
+    }}>
+      <App />
+    </SWRConfig>
   </React.StrictMode>,
 );
